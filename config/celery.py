17be3a444c19e0a7b22c6d3f1fbc5696cd534d5f
artur <youzen0you@gmail.com>
2024-07-18 00:44:06 +0500

    fix

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,10 @@
+from __future__ import absolute_import, unicode_literals
+
 import os
 
 from celery import Celery
+
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -8,4 +12,8 @@ app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
